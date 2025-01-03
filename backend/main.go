f03b2b58fb0092c981d3af1e04b17e3dae9703ba
Ben Petrillo <benpetrillo.bp@gmail.com>
2025-01-02 22:53:53 -0500

    chore: backend/health setup

---
+++ backend/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/GenerateNU/platemate/internal/server"
+	"log"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+
+	app := server.New()
+
+	go func() {
+		if err := app.Listen(":8080"); err != nil {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+
+	slog.Info("Shutting down server")
+	if err := app.Shutdown(); err != nil {
+		slog.Error("failed to shutdown server", "error", err)
+	}
+
+	slog.Info("Server shutdown")
+}
