51ee83a62e661e4b84f2809b5e647ce48acd9afe
garrettladley <garrett.ladley14@gmail.com>
2025-01-08 15:48:31 -0500

    fix: rm uri.go

--- backend/internal/storage/mongo/uri.go
+++
@@ -1,9 +0,0 @@
-package mongo
-
-import "fmt"
-
-const placeholderUri string = "mongodb+srv://%s:%s@development.t8bgq.mongodb.net/?retryWrites=true&w=majority&appName=%s"
-
-func GenerateAtlasURI(user string, pass string, cluster string) string {
-	return fmt.Sprintf(placeholderUri, user, pass, cluster)
-}
