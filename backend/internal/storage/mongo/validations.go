1cd5d45bc12b8843b474ff9513d02282e92ed3e4
michelleli04 <michelle6465li@gmail.com>
2025-01-17 21:24:03 -0500

    feat: add validator for menu-items

--- backend/internal/storage/mongo/validations.go
+++ backend/internal/storage/mongo/validations.go
@@ -9,6 +9,7 @@ var (
 	validations = map[string]bson.M{
 		"health": healthValidator,
 		"users":  usersValidator,
+		"menu-items": menuItemsValidator,
 	}
 	defaultValidator = bson.M{
 		"bsonType":             "object",
@@ -38,4 +39,85 @@ var (
 			},
 		},
 	}
+	menuItemsValidator = bson.M{
+		"bsonType": "object",
+		"required": []string{
+			"_id", 
+			"name", 
+			"avgRating", 
+			"picture",
+			"reviews", 
+			"description", 
+			"location", 
+			"tags", 
+			"dietaryRestrictions",
+		},
+		"properties": bson.M{
+			"_id": bson.M{
+				"bsonType": "objectId",
+			},
+			"name": bson.M{
+				"bsonType": "string",
+			},
+			"picture": bson.M{
+				"bsonType": "string",
+			},
+			"avgRating": bson.M{
+				"bsonType": "object",
+				"properties": bson.M{
+					"portion": bson.M{
+						"bsonType": bson.M{"$in": []string{"double", "null"}},
+						"description": "Rating for portion size",
+					},
+					"taste": bson.M{
+						"bsonType": bson.M{"$in": []string{"double", "null"}},
+						"description": "Rating for taste",
+					},
+					"value": bson.M{
+						"bsonType": bson.M{"$in": []string{"double", "null"}},
+						"description": "Rating for value",
+					},
+					"overall": bson.M{
+						"bsonType": bson.M{"$in": []string{"double", "null"}},
+						"description": "Overall rating",
+					},
+					"return": bson.M{
+						"bsonType": bson.M{"$in": []string{"bool", "null"}},
+						"description": "Would you return?",
+					},
+				},
+			},
+			"reviews": bson.M{
+				"bsonType": "array",
+				"items": bson.M{
+					"bsonType": "string",
+				},
+			},
+			"description": bson.M{
+				"bsonType": "string",
+			},
+			"location": bson.M{
+				"bsonType": "array",
+				"items": bson.M{
+					"bsonType": "double",
+				},
+				"minItems": 2,
+				"maxItems": 2,
+			},
+			"tags": bson.M{
+				"bsonType": "array",
+				"items": bson.M{
+					"bsonType": "string",
+				},
+			},
+			"dietaryRestrictions": bson.M{
+				"bsonType": "array",
+				"items": bson.M{
+					"bsonType": "string",
+				},
+			},
+		},
+	}
+	
+		
 )
