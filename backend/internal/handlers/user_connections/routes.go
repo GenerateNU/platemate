2f3e89de7e62d7412ce0c493e9aca0042f52ea67
David Yu <yu.dav@northeastern.edu>
2025-01-27 21:09:21 -0500

    feat: setup CRUD routes

--- backend/internal/handlers/user_connections/routes.go
+++ backend/internal/handlers/user_connections/routes.go
@@ -9,12 +9,19 @@ func Routes(app *fiber.App, collections map[string]*mongo.Collection) {
 	service := newService(collections)
 	handler := Handler{service}
 
+	// Group under API Version 1
 	apiV1 := app.Group("/api/v1")
 
 	// User connections endpoints
 	user := apiV1.Group("/user")
+
+	// Following/Followers endpoints
 	user.Get("/followers", handler.GetFollowers)
-	user.Get("/item/:id/followReviews", handler.GetFollowReviews)
+	user.Get("/following", handler.GetFollowing)
 	user.Post("/follow", handler.FollowUser)
 	user.Delete("/follow", handler.UnfollowUser)
+
+	// User review-related endpoints that involve following
+	user.Get("/feed", handler.GetFollowingFeed)
+	user.Get("/item/:id/following-reviews", handler.GetFollowingReviewsForItem)
 }
