e9db6edc271dbb1a015b20060b52bb9cdd529cbf
Dylan Anctil <134968796+danctila@users.noreply.github.com>
2025-02-08 11:54:08 -0500

    feat: create baseline components (#49)

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -13,7 +13,7 @@ export default function HomeScreen() {
             headerImage={<Image source={require("@/assets/images/partial-react-logo.png")} style={styles.reactLogo} />}>
             <ThemedView style={styles.titleContainer}>
                 <ThemedText type={"title"} style={{ fontFamily: "Outfit" }}>
-                    PlatMate - React Native Boilerplate
+                    PlateMate - React Native Boilerplate
                 </ThemedText>
             </ThemedView>
             <ThemedView>
---
+++ frontend/components/Button.tsx
@@ -0,0 +1,47 @@
+import React, { PropsWithChildren } from "react";
+import {
+    TouchableOpacity,
+    Text,
+    type StyleProp,
+    type TextStyle,
+    type ViewStyle,
+    type TouchableOpacityProps,
+} from "react-native";
+
+export interface ButtonProps extends TouchableOpacityProps, PropsWithChildren<{}> {
+    title?: string;
+    containerStyle?: StyleProp<ViewStyle>;
+    textStyle?: StyleProp<TextStyle>;
+}
+
+export function Button(props: ButtonProps) {
+    const { onPress, title, containerStyle, textStyle, children, ...rest } = props;
+
+    return (
+        // Fill in rest with whatever you want to pass to the View or TouchableOpacity component!
+        <TouchableOpacity onPress={onPress} style={containerStyle} {...rest}>
+            {title ? <Text style={textStyle}>{title}</Text> : children}
+        </TouchableOpacity>
+    );
+}
+
+// Example usage:
+
+// import { Button } from "@/components/Button";
+// import { Alert, View } from "react-native";
+{
+    /* <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Button
+        title="Click Me!"
+        onPress={() => Alert.alert("Button Pressed!")}
+        containerStyle={{
+          padding: 12,
+          borderRadius: 8,
+          // more styles etc...
+        }}
+        textStyle={{
+          // color: "#FFFFFF",
+        }}
+      />
+    </View> */
+}
