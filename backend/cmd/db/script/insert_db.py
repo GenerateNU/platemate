5a53a80cd9a906dd09563cd289f0f8abe46effcb
Sierra Welsch <119388792+sierrawelsch@users.noreply.github.com>
2025-02-13 14:12:48 -0500

    feat: user & restaurant mock data (#48)
    Co-authored-by: Ben Petrillo <benpetrillo.bp@gmail.com>

--- backend/cmd/db/script/insert_db.py
+++ backend/cmd/db/script/insert_db.py
@@ -1,14 +1,21 @@
 from pymongo import MongoClient
-from restaurant_setup import convert_to_mongo_db_format, load_env_variable
+from restaurant_setup import convert_to_mongo_db_format, load_env_variable, generate_mock_restaurant_data
+from users_setup import generate_user_data
 
 mongo_db_uri = load_env_variable("backend/.env", "MONGO_DB_URI")
 client = MongoClient(mongo_db_uri)
-db = client['dev-restaurants-api']
+db = client['Production']
 collection = db['restaurants']
 
 def insert_db(restaurants):
     for place in restaurants:
             collection.insert_one(place)
+            
+def delete_documents():
+    collection.delete_many({})
 
-restaurants_for_mongo = convert_to_mongo_db_format()
-insert_db(restaurants_for_mongo)
+#restaurants_for_mongo = convert_to_mongo_db_format()
+# users_for_mongo = generate_user_data()
+mock_restaurant_data = generate_mock_restaurant_data()
+insert_db(mock_restaurant_data)
+# delete_documents()
