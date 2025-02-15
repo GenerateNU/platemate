35a11ebf1d82a3eb84ec62044a755413a69ec9eb
Ben Petrillo <benpetrillo.bp@gmail.com>
2025-02-15 13:43:15 -0500

    feat: icon input integration

--- frontend/__tests__/AboutScreen.test.tsx
+++ frontend/__tests__/AboutScreen.test.tsx
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/react-native";
 
-import AboutScreen from "@/app/about";
+import AboutScreen from "@/app/feed";
 
 describe("home screen", () => {
     test("text renders correctly", () => {
