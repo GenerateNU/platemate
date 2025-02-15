12d7ca4ace25518db83e5d94abf3aab9eab97e0e
Abhik Ray <abhikaboy@gmail.com>
2025-02-15 16:16:52 -0500

    refactor: search uses ref

--- frontend/components/SearchBox.tsx
+++ frontend/components/SearchBox.tsx
@@ -1,17 +1,52 @@
-import React from "react";
+import { useRecentSearch } from "../hooks/useRecentSearch";
+import React, { useEffect, useRef } from "react";
 import { TextInput, TextInputProps, StyleSheet, View } from "react-native";
 
 interface SearchBoxProps extends TextInputProps {
     value: string;
+    recent?: boolean;
+    name?: string;
+    onSubmit: () => void;
     onChangeText: (text: string) => void;
     icon?: React.ReactNode;
 }
 
-export function SearchBox({ value, onChangeText, icon, ...rest }: SearchBoxProps) {
+export function SearchBox({ value, onChangeText, onSubmit, icon, recent, name, ...rest }: SearchBoxProps) {
+    const { getRecents, appendSearch } = useRecentSearch(name);
+    let recents: any = useRef([]); // TODO: fix this type
+    useRef(recents);
+    useEffect(() => {
+        if (recent) {
+            recents.current = getRecents();
+        }
+    }, [name]);
+
+    const onSubmitEditing = () => {
+        if (recent) {
+            appendSearch(value);
+        }
+        onSubmit();
+    };
+
     return (
-        <View style={styles.container}>
-            <TextInput value={value} onChangeText={onChangeText} {...rest} style={styles.input} />
-            {icon && icon}
+        <View>
+            <View style={styles.container}>
+                <TextInput
+                    onSubmitEditing={onSubmitEditing}
+                    value={value}
+                    onChangeText={onChangeText}
+                    {...rest}
+                    style={styles.input}
+                />
+                {icon && icon}
+            </View>
+            {recent && (
+                <View>
+                    {recents.current.map((term: string, index: number) => {
+                        return <View key={index} />;
+                    })}
+                </View>
+            )}
         </View>
     );
 }
--- frontend/hooks/useRecentSearch.ts
+++ frontend/hooks/useRecentSearch.ts
@@ -2,11 +2,14 @@ import { useEffect, useState } from "react";
 import { useColorScheme } from "react-native";
 import asyncStorage from "@react-native-async-storage/async-storage";
 
-export function useRecentSearch(searchSet: string) {
+export function useRecentSearch(searchSet: string = "") {
     // two functions, append recent and get recents
     const [recents, setRecents] = useState<string[]>([]);
     const MAX_RECENTS = 6;
     useEffect(() => {
+        if (searchSet === "") {
+            setRecents(() => []);
+        }
         // @ts-ignore
         setRecents(async () => {
             const recents = await asyncStorage.getItem(searchSet);
@@ -19,6 +22,9 @@ export function useRecentSearch(searchSet: string) {
     }, [searchSet]);
 
     const appendSearch = (search: string) => {
+        if (searchSet === "") {
+            return;
+        }
         setRecents((prev) => {
             let newRecents = [...prev, search];
             newRecents = prev.slice(0, MAX_RECENTS - 1);
