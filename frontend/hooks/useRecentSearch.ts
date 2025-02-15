5da9a3fef8381302c86b970c3ff32ca9bd63443f
Abhik Ray <abhikaboy@gmail.com>
2025-02-15 15:13:33 -0500

    feat: recent search hook

---
+++ frontend/hooks/useRecentSearch.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { useColorScheme } from "react-native";
+import asyncStorage from "@react-native-async-storage/async-storage";
+
+export function useRecentSearch(searchSet: string) {
+    // two functions, append recent and get recents
+    const [recents, setRecents] = useState<string[]>([]);
+    const MAX_RECENTS = 6;
+    useEffect(() => {
+        // @ts-ignore
+        setRecents(async () => {
+            const recents = await asyncStorage.getItem(searchSet);
+            if (recents) {
+                return JSON.parse(recents);
+            } else {
+                return [];
+            }
+        });
+    }, [searchSet]);
+
+    const appendSearch = (search: string) => {
+        setRecents((prev) => {
+            let newRecents = [...prev, search];
+            newRecents = prev.slice(0, MAX_RECENTS - 1);
+            asyncStorage.setItem(searchSet, JSON.stringify(newRecents));
+            return newRecents;
+        });
+    };
+
+    const getRecents = () => {
+        return recents;
+    };
+
+    return { getRecents, appendSearch };
+}
