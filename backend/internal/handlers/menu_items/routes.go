da059f04455b5761e53688a42b3dfb40eac431a1
Dylan Anctil <134968796+danctila@users.noreply.github.com>
2025-03-15 23:13:14 -0400

    fix: reorder path declarations in menu item routes

--- backend/internal/handlers/menu_items/routes.go
+++ backend/internal/handlers/menu_items/routes.go
@@ -12,7 +12,7 @@ func Routes(app *fiber.App, collections map[string]*mongo.Collection) {
 	service := newService(collections)
 	handler := Handler{service}
 	menuGroup := app.Group("/api/v1/menu-items")
-
+	menuGroup.Get("/popular-with-friends", handler.GetPopularWithFriends)
 	menuGroup.Post("/", handler.CreateMenuItem)
 	menuGroup.Get("/", handler.GetMenuItems)
 	menuGroup.Get("/:id", handler.GetMenuItemById)
@@ -21,6 +21,5 @@ func Routes(app *fiber.App, collections map[string]*mongo.Collection) {
 	menuGroup.Delete("/:id", handler.DeleteMenuItem)
 	menuGroup.Get("/:id/reviews", handler.GetMenuItemReviews)
 	menuGroup.Get("/:id/review-pictures", handler.GetMenuItemReviewPictures)
-	menuGroup.Get("/popular-with-friends", handler.GetPopularWithFriends)
 
 }
