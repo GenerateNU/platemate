58b618e33d57373c19c494fe37855bed5c378970
michelleli04 <michelle6465li@gmail.com>
2025-03-29 16:07:37 -0400

    feat: connect feed to filters and use a context

--- frontend/app/feed.tsx
+++ frontend/app/feed.tsx
@@ -1,15 +1,20 @@
 import { Dimensions, StyleSheet, View } from "react-native";
 
 import { ThemedView } from "@/components/themed/ThemedView";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { SearchBox } from "@/components/SearchBox";
 import { SearchIcon } from "@/components/icons/Icons";
 import FeedTabs from "@/components/Feed/FeedTabs";
 import FeedContent from "@/components/Feed/FeedContent";
+import { FilterContext } from "@/context/filter-context";
 
 export default function FeedScreen() {
-    const [searchText, setSearchText] = React.useState("");
     const [activeTab, setActiveTab] = useState(0);
+    const context = useContext(FilterContext);
+    if (!context) {
+        throw new Error("Filter component must be used within a FilterProvider");
+    }
+    const { handleSearch, searchText, setSearchText } = context;
 
     return (
         <ThemedView style={styles.textContainer}>
@@ -19,9 +24,10 @@ export default function FeedScreen() {
                     placeholder={"What are you hungry for?"}
                     recent={true}
                     name={"general"}
-                    onSubmit={() => console.log("submit")}
+                    onSubmit={handleSearch}
                     value={searchText}
                     onChangeText={(text) => setSearchText(text)}
+                    filter={true}
                 />
                 <View style={{ marginTop: 24, marginBottom: 24 }}>
                     <FeedTabs tabs={["Friends", "Recommended"]} activeTab={activeTab} setActiveTab={setActiveTab} />
