869bd80a9690af9d951a2f6da67d262495de8194
Abhik Ray <abhikaboy@gmail.com>
2025-04-11 17:04:45 -0400

    merge 2

--- frontend/app/(restaurant)/[id].tsx
+++ frontend/app/(restaurant)/[id].tsx
@@ -20,11 +20,8 @@ import { Skeleton } from "moti/skeleton";
 import { getRestaurantReviews, getRestaurantReviewsByUser, getReviews } from "@/api/reviews";
 import { TReview } from "@/types/review";
 import { useUser } from "@/context/user-context";
-<<<<<<< HEAD
 import { TMenuItem } from "@/types/menu-item";
 import { getMenuItemsByRestaurant } from "@/api/menu-items";
-=======
->>>>>>> origin
 
 export default function Route() {
     const restaurantTags = ["Fast Food", "Fried Chicken", "Chicken Sandwiches", "Order Online"];
@@ -63,13 +60,10 @@ export default function Route() {
             setReviews(res);
         });
 
-<<<<<<< HEAD
         getMenuItemsByRestaurant(id).then((res) => {
             setMenuItems(res);
         });
 
-=======
->>>>>>> origin
         getRestaurantReviewsByUser(id, user.id).then((res) => {
             console.log(res);
             setMyReviews(res);
